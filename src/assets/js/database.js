1df6c5a91a3b593b3d7ebd05ef79a4d2f8c1c6df
Matthew Abtin <staalkoper@gmail.com>
2020-06-17 17:34:41 +0200

    test

--- src/assets/js/database.js
+++ src/assets/js/database.js
@@ -4,10 +4,6 @@ class main {
     constructor() {
         this.name = "Matthew"
     }
-
-    fun (name) {
-        console.log(name)
-    }
     greet () {
         console.log(`Hello ${this.name}`)
     }
