9e6f2a9e89c4d314ebac34a46d4fe08a7f95b3c9
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-06-18 02:38:53 +0200

    ajhsbdfadfjsvb

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
 from flask import request, url_for
 from flask_api import FlaskAPI, status, exceptions
+import psycopg2
+import urllib.parse as urlparse
 
+result = urlparse.urlparse(
+    "postgres://noodles:Jupiter22@noodles-bot.cimffc8jntkt.us-east-1.rds.amazonaws.com:5432/postgres"
+)
+
+conn = psycopg2.connect(
+    database=result.path[1:],
+    user=result.username,
+    password=result.password,
+    host=result.hostname,
+)
+
+cur = conn.cursor()
 app = FlaskAPI(__name__)
 
-notes = {
-    0: 'do the shopping',
-    1: 'build the codez',
-    2: 'paint the door',
-}
+
+def fetch():
+    cur.execute("SELECT * FROM api.commands")
+
+    commands_sql = cur.fetchall()
+    commands = {}
+
+    for row in commands_sql:
+        commands[row[0]] = row[1]
+
+    return commands
+
+
+commands = fetch()
 
 
 def note_repr(key):
     return {
         'url': request.host_url.rstrip('/') + url_for('notes_detail', key=key),
-        'text': notes[key]
+        'text': commands[key]
     }
 
 
@@ -24,12 +47,14 @@ def notes_list():
     """
     if request.method == 'POST':
         note = str(request.data.get('text', ''))
-        idx = max(notes.keys()) + 1
-        notes[idx] = note
+        idx = max(commands.keys()) + 1
+        commands[idx] = note
+        cur.execute(f"INSERT INTO api.commands(id, command) VALUES ({idx}, '{note}')")
+        conn.commit()
         return note_repr(idx), status.HTTP_201_CREATED
 
     # request.method == 'GET'
-    return [note_repr(idx) for idx in sorted(notes.keys())]
+    return [note_repr(idx) for idx in sorted(commands.keys())]
 
 
 @app.route("/<int:key>/", methods=['GET', 'PUT', 'DELETE'])
@@ -39,15 +64,15 @@ def notes_detail(key):
     """
     if request.method == 'PUT':
         note = str(request.data.get('text', ''))
-        notes[key] = note
+        commands[key] = note
         return note_repr(key)
 
     elif request.method == 'DELETE':
-        notes.pop(key, None)
+        commands.pop(key, None)
         return '', status.HTTP_204_NO_CONTENT
 
     # request.method == 'GET'
-    if key not in notes:
+    if key not in commands:
         raise exceptions.NotFound()
     return note_repr(key)
 
