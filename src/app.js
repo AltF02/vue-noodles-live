50bd6b8e89418670223b487204fc22f071ba1614
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-06-22 07:17:23 +0200

    setting up api

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 8383
 const db = require('./db/queries')
 
 app.use(bodyParser.json())
